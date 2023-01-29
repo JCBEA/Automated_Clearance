5e6c90f66f66f1f1e006febf05cd044ce25e4b0f
JCBEA <111287289+JCBEA@users.noreply.github.com>
2023-01-29 14:02:36 +0800

    login_style

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="Styles/login.css">
+</head>
+<body>
+<div class="wrap">
+    <div class="title">
+        Online Clearance
+    </div>
+		<div class="avatar">
+      <img src="images/login_img/dwcl-logo.jpg">
+		</div>
+		<input type="text" placeholder="username" required>
+		<div class="bar">
+			<i></i>
+		</div>
+		<input type="password" placeholder="password" required>
+		<button>Sign in</button>
+	</div>
+</body>
+</html>
